12504140b411fc058562e5047fbcc2c46f173f05
jhs512 <jangka512@gmail.com>
2023-12-21 14:34:29 +0900

    work

--- src/main/java/com/ll/mb/domain/product/order/entity/OrderItem.java
+++ src/main/java/com/ll/mb/domain/product/order/entity/OrderItem.java
@@ -1,12 +1,9 @@
-package com.ll.mb.domain.product.cart.entity;
+package com.ll.mb.domain.product.order.entity;
 
-
-import com.ll.mb.domain.member.member.entity.Member;
 import com.ll.mb.domain.product.product.entity.Product;
 import com.ll.mb.global.jpa.BaseEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
 import lombok.*;
 
 import static lombok.AccessLevel.PROTECTED;
@@ -18,10 +15,9 @@ import static lombok.AccessLevel.PROTECTED;
 @Setter
 @Getter
 @ToString(callSuper = true)
-public class CartItem extends BaseEntity {
+public class OrderItem extends BaseEntity {
     @ManyToOne
-    private Member buyer;
-    @OneToOne
+    private Order order;
+    @ManyToOne
     private Product product;
 }
-
