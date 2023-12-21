84843b7b5226a4130a796a89f1fc0827c3891073
jhs512 <jangka512@gmail.com>
2023-12-21 13:15:55 +0900

    cartItem

---
+++ src/main/java/com/ll/mb/domain/product/cart/entity/CartItem.java
@@ -0,0 +1,27 @@
+package com.ll.mb.domain.product.cart.entity;
+
+
+import com.ll.mb.domain.member.member.entity.Member;
+import com.ll.mb.domain.product.product.entity.Product;
+import com.ll.mb.global.jpa.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import lombok.*;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Builder
+@AllArgsConstructor(access = PROTECTED)
+@NoArgsConstructor(access = PROTECTED)
+@Setter
+@Getter
+@ToString(callSuper = true)
+public class CartItem extends BaseEntity {
+    @ManyToOne
+    private Member member;
+    @OneToOne
+    private Product product;
+}
+
