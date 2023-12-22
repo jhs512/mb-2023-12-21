f9586e1b0c11c99a366fed2a1fbed30570602cff
jhs512 <jangka512@gmail.com>
2023-12-22 14:38:00 +0900

    work

--- src/main/java/com/ll/mb/domain/member/member/service/MemberService.java
+++ src/main/java/com/ll/mb/domain/member/member/service/MemberService.java
@@ -14,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Optional;
 
@@ -28,7 +29,13 @@ public class MemberService {
 
     @Transactional
     public RsData<Member> join(String username, String password, String nickname) {
-        return join(username, password, nickname, null);
+        return join(username, password, nickname, "");
+    }
+
+    @Transactional
+    public RsData<Member> join(String username, String password, String nickname, MultipartFile profileImg) {
+        String profileImgFilePath = Ut.file.toFile(profileImg, AppConfig.getTempDirPath());
+        return join(username, password, nickname, profileImgFilePath);
     }
 
     @Transactional
