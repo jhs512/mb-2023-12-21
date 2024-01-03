7881802e61fcd932b996947ed26278b8da5fb13f
jhs512 <jangka512@gmail.com>
2024-01-03 10:19:53 +0900

    work

--- src/main/java/com/ll/mb/global/rq/Rq.java
+++ src/main/java/com/ll/mb/global/rq/Rq.java
@@ -1,5 +1,6 @@
 package com.ll.mb.global.rq;
 
+import com.ll.mb.domain.global.exceptions.GlobalException;
 import com.ll.mb.domain.member.member.entity.Member;
 import com.ll.mb.domain.member.member.service.MemberService;
 import com.ll.mb.global.rsData.RsData;
@@ -9,6 +10,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -22,6 +24,7 @@ import java.util.Optional;
 @Component
 @RequestScope
 @RequiredArgsConstructor
+@Slf4j
 public class Rq {
     private final HttpServletRequest request;
     private final HttpServletResponse response;
@@ -55,6 +58,15 @@ public class Rq {
         }
 
         return sb.toString();
+    }
+
+    public String historyBack(GlobalException ex) {
+        String exStr = Ut.exception.toString(ex);
+
+        request.setAttribute("exStr", exStr);
+        log.debug(exStr);
+
+        return historyBack(ex.getRsData().getMsg());
     }
 
     public String historyBack(String msg) {
