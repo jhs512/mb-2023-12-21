94b05026a5c967e9b00686fd4a5d92a1e1449ee8
jhs512 <jangka512@gmail.com>
2024-01-04 10:19:26 +0900

    work

--- src/main/java/com/ll/mb/domain/cash/withdraw/controller/WithdrawController.java
+++ src/main/java/com/ll/mb/domain/cash/withdraw/controller/WithdrawController.java
@@ -1,14 +1,19 @@
 package com.ll.mb.domain.cash.withdraw.controller;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
 @RequestMapping("/withdraw")
+@Controller
 @RequiredArgsConstructor
 @Slf4j
 @PreAuthorize("isAuthenticated()")
@@ -16,5 +21,20 @@ public class WithdrawController {
     @GetMapping("/apply")
     public String showApply() {
         return "domain/cash/withdraw/apply";
+    }
+
+    public record ApplyForm(
+            @NotNull long cash,
+            @NotBlank String bankName,
+            @NotBlank String bankAccountNo
+    ) {
+    }
+
+    @PostMapping("/apply")
+    @ResponseBody
+    public String apply(
+            @Valid ApplyForm form
+    ) {
+        return form.toString();
     }
 }
