7d3773f770cd8a199d444d72fcc0a8f618431679
jhs512 <jangka512@gmail.com>
2024-01-10 15:46:13 +0900

    work

--- src/main/java/com/ll/mb/domain/product/order/repository/OrderItemRepository.java
+++ src/main/java/com/ll/mb/domain/product/order/repository/OrderItemRepository.java
@@ -3,5 +3,9 @@ package com.ll.mb.domain.product.order.repository;
 import com.ll.mb.domain.product.order.entity.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+    List<OrderItem> findByOrderPayDateBetweenOrderByIdDesc(LocalDateTime startDate, LocalDateTime endDate);
 }
--- src/main/java/com/ll/mb/domain/rebate/rebate/controller/AdmRebateController.java
+++ src/main/java/com/ll/mb/domain/rebate/rebate/controller/AdmRebateController.java
@@ -1,7 +1,5 @@
 package com.ll.mb.domain.rebate.rebate.controller;
 
-import com.ll.mb.domain.product.order.entity.OrderItem;
-import com.ll.mb.domain.product.order.service.OrderService;
 import com.ll.mb.domain.rebate.rebate.service.RebateService;
 import com.ll.mb.global.rq.Rq;
 import lombok.RequiredArgsConstructor;
@@ -10,13 +8,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
-
 @Controller
 @RequestMapping("/adm/rebate")
 @RequiredArgsConstructor
 public class AdmRebateController {
-    private final OrderService orderService;
     private final RebateService rebateService;
     private final Rq rq;
 
@@ -26,10 +21,10 @@ public class AdmRebateController {
     }
 
     @PostMapping("/make")
-    public String make() {
-        List<OrderItem> orderItems = orderService.findItems();
-
-        rebateService.make(orderItems);
+    public String make(
+            String yearMonth
+    ) {
+        rebateService.make(yearMonth);
 
         return rq.redirect("/adm/rebate/make", "정산데이터를 생성했습니다.");
     }
