4e77b595ea5d3ba4561b0d884b2540875207eef9
jhs512 <jangka512@gmail.com>
2024-01-11 09:30:52 +0900

    work

--- src/main/java/com/ll/mb/domain/rebate/rebate/entity/RebateItem.java
+++ src/main/java/com/ll/mb/domain/rebate/rebate/entity/RebateItem.java
@@ -4,9 +4,7 @@ import com.ll.mb.domain.member.member.entity.Member;
 import com.ll.mb.domain.product.order.entity.OrderItem;
 import com.ll.mb.domain.product.product.entity.Product;
 import com.ll.mb.global.jpa.BaseTime;
-import jakarta.persistence.Entity;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -24,12 +22,16 @@ public class RebateItem extends BaseTime {
     private LocalDateTime eventDate; // 판매(구매)가 발생한 날짜
     private LocalDateTime rebateDate; // 정산일
     @OneToOne
+    @JoinColumn(foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
     private OrderItem orderItem; // 주문상품
     @ManyToOne
+    @JoinColumn(foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
     private Member seller; // 판매자
     @ManyToOne
+    @JoinColumn(foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
     private Member buyer; // 구매자
     @ManyToOne
+    @JoinColumn(foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
     private Product product; // 상품
     private long payPrice; // 결제금액
     private double rebateRate; // 정산율
